2b8afad097a212e43c8630d5890a2f7dec2cab62
Rshalika <rshal13@freeuni.edu.ge>
2016-08-18 21:52:19 +0400

    add http methods

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using test.Models;
 
 namespace test
 {
@@ -30,6 +30,7 @@ namespace test
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
